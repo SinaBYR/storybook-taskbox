f6a4d9e040488a1ce5f68a1fc860976f27563500
SinaBYR <sina.beyraghdar1@gmail.com>
2022-01-23 22:23:32 +0330

    Fix test code after adding InboxScreen container to App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Taskbox span inside heading 1 tag', () => {
+  const { container } = render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  // expect(container.querySelector('.page.lists-show '))
+  //   .not.toBe(null);
+  const spanInsideHeadingOneTag = screen.getByText('Taskbox')
+  expect(spanInsideHeadingOneTag).toHaveClass('title-wrapper');
 });
