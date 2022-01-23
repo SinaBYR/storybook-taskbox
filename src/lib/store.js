cc38927d014dbecb88ade25ae345e97b4489a4bd
SinaBYR <sina.beyraghdar1@gmail.com>
2022-01-23 22:18:39 +0330

    Add AppStateSlice (error) to the redux store

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,5 +1,18 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+const AppStateSlice = createSlice({
+  name: 'appState',
+  initialState: '',
+  reducers: {
+    updateAppState: (state, action) => {
+      return {
+        ...state,
+        isError: action.payload
+      }
+    }
+  }
+})
+
 const defaultTasks = [
   { id: '1', title: 'Something', state: 'TASK_INBOX' },
   { id: '2', title: 'Something more', state: 'TASK_INBOX' },
@@ -21,11 +34,14 @@ const TasksSlice = createSlice({
   }
 })
 
+// export actions
 export const { updateTaskState } = TasksSlice.actions;
+export const { updateAppState } = AppStateSlice.actions;
 
 const store = configureStore({
   reducer: {
-    tasks: TasksSlice.reducer
+    tasks: TasksSlice.reducer,
+    isError: AppStateSlice.reducer
   }
 })
 
